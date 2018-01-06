450ad4686003f83af83f5cf0c70b12f8f8a60ba6
MtZero <zhonglsh7@mail2.sysu.edu.cn>
2018-01-06 20:01:19 +0800

    update

--- app/src/main/java/com/example/acera/theforum/RegisterActivity.kt
+++ app/src/main/java/com/example/acera/theforum/RegisterActivity.kt
@@ -1,17 +1,35 @@
 package com.example.acera.theforum
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
+import com.dd.processbutton.iml.ActionProcessButton
+import com.example.acera.theforum.Model.Json
+import com.example.acera.theforum.NetworkService.ServiceFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_register.*
+import io.reactivex.Observer
+import io.reactivex.disposables.Disposable
 
 class RegisterActivity : AppCompatActivity()
 {
+
+    var msg: Json.Message? = null
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        registerButton.setMode(ActionProcessButton.Mode.ENDLESS)
+        registerButton.setColorScheme(
+                getColor(R.color.material_light_blue_700),
+                getColor(R.color.material_red_700),
+                getColor(R.color.material_orange_700),
+                getColor(R.color.material_light_green_700))
     }
 
     fun registerButtonClick(view: View)
@@ -59,21 +77,77 @@ class RegisterActivity : AppCompatActivity()
             focusView!!.requestFocus()
         } else
         {
-            registerButton.progress = 1
-            registerButton.isEnabled = false
-            registerUserLayout.isEnabled = false
-            registerPasswordLayout.isEnabled = false
-            //TODO("register action")
-            registerButton.progress = 100
-            try
-            {
-                Thread.sleep(2000)
-            } catch (e: InterruptedException)
-            {
-                e.printStackTrace()
-            }
+//            registerButton.progress = 1
+//            registerButton.isEnabled = false
+//            registerUserLayout.isEnabled = false
+//            registerPasswordLayout.isEnabled = false
+//            registerConfirmLayout.isEnabled = false
+//            //TODO("register action")
+//            registerButton.progress = 100
+//            try
+//            {
+//                Thread.sleep(2000)
+//            } catch (e: InterruptedException)
+//            {
+//                e.printStackTrace()
+//            }
+            registerAction(userStr, passwordStr)
             finish()
 
         }
+    }
+
+    private fun registerAction(username: String, password: String)
+    {
+        ServiceFactory.myService
+                .register(Json.User(null, username, password, null, null))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<Json.Message>
+                {
+                    override fun onNext(t: Json.Message) {
+                        msg = t
+                    }
+
+                    override fun onError(e: Throwable) {
+                        registerFailed("Register failed!")
+                        e.printStackTrace()
+                    }
+
+                    override fun onComplete() {
+                        if (msg!!.state == "1")
+                        {
+                            registerFailed(msg!!.message)
+                        } else
+                        {
+                            registerButton.progress = 100
+                            Toast.makeText(this@RegisterActivity, "Register successfully!", Toast.LENGTH_SHORT).show()
+                            Handler().postDelayed({ toLoginActivity() }, 1500)
+                        }
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        registerButton.progress = 1
+                        registerButton.isEnabled = false
+                        registerUserLayout.isEnabled = false
+                        registerPasswordLayout.isEnabled = false
+                        registerConfirmLayout.isEnabled = false
+                    }
+                })
+    }
+
+    private fun registerFailed(message: String)
+    {
+        Toast.makeText(this@RegisterActivity, message, Toast.LENGTH_SHORT).show()
+        registerButton.progress = -1
+        registerButton.isEnabled = true
+        registerUserLayout.isEnabled = true
+        registerPasswordLayout.isEnabled = true
+        registerConfirmLayout.isEnabled = true
+    }
+
+    private fun toLoginActivity()
+    {
+        finish()
     }
 }
